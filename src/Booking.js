e88435ca6e5721e54f2a7711eb59a25b51b9e066
Jonas Holbech <jh@jonasholbech.dk>
2023-04-17 12:02:03 +0200

    === to <=

--- src/Booking.js
+++ src/Booking.js
@@ -9,31 +9,31 @@ class Booking {
       {
         area: "Svartheim",
         spots: 400,
-        available: rndBetween(0, 400),
+        available: rndBetween(1, 400),
         direction: -1,
       },
       {
         area: "Nilfheim",
         spots: 300,
-        available: rndBetween(0, 300),
+        available: rndBetween(1, 300),
         direction: -1,
       },
       {
         area: "Helheim",
         spots: 100,
-        available: rndBetween(0, 100),
+        available: rndBetween(1, 100),
         direction: -1,
       },
       {
         area: "Muspelheim",
         spots: 200,
-        available: rndBetween(0, 200),
+        available: rndBetween(1, 200),
         direction: -1,
       },
       {
         area: "Alfheim",
         spots: 250,
-        available: rndBetween(0, 250),
+        available: rndBetween(1, 250),
         direction: -1,
       },
     ];
@@ -98,20 +98,15 @@ class Booking {
     }
   }
   tick() {
-    /* this.timeoutIds = this.timeoutIds.filter(
-      (oneTimeout) => Date.now() < oneTimeout.expires
-    ); */
     if (Math.random() * 100 < this.fest.eventChance) {
       const areaIndex = rndBetween(0, this.areas.length - 1);
       this.areas[areaIndex].available += this.areas[areaIndex].direction;
-      if (this.areas[areaIndex].available === 0) {
+      if (this.areas[areaIndex].available <= 0) {
         this.areas[areaIndex].direction = 1;
+        this.areas[areaIndex].available = 0;
       } else if (this.areas[areaIndex].available > 20) {
         this.areas[areaIndex].direction = -1;
       }
-      /* console.log(
-        this.areas.map((a) => a.area + ": " + a.available).join("\n")
-      ); */
     }
   }
 }
