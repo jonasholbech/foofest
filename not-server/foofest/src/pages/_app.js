023631f6c39f8a5579211dd9e1b8a98e9fc18f07
Federico Barbieri <federicobarbieri54@gmail.com>
2023-05-17 23:18:56 +0200

    camping all costs included

--- not-server/foofest/src/pages/_app.js
+++ not-server/foofest/src/pages/_app.js
@@ -5,10 +5,11 @@ import { useState } from 'react'
 
 export default function App({ Component, pageProps }) {
   const [costOfTickets, setCostOfTickets] = useState(0)
+  const [ticketsPlusTents, setTicketsPlusTents] = useState(0);
 
   return ( 
     <>
-      <TicketsContext.Provider value={{costOfTickets, setCostOfTickets}}>
+      <TicketsContext.Provider value={{costOfTickets, setCostOfTickets, ticketsPlusTents, setTicketsPlusTents}}>
              <Layout>
             <Component {...pageProps} />
             </Layout>
