a595e4b08cd9b2107a707b443767e501a301cc53
Federico Barbieri <federicobarbieri54@gmail.com>
2023-05-26 15:01:29 -0300

    camping add ons disabled btn done

--- not-server/foofest/src/pages/buyingStage/campingAddOns/index.js
+++ not-server/foofest/src/pages/buyingStage/campingAddOns/index.js
@@ -44,12 +44,16 @@ const [personalTents, setPersonalTents] = useState(0);
 function addPersonalTent(){
   if(personalTents < 4){
   setPersonalTents(old => old+1);
+  setNumOfTents(old => old+1)
+
 }
 }
 
 function subtractPersonalTent(){
   if(personalTents > 0){
   setPersonalTents(old => old-1);
+  setNumOfTents(old => old-1)
+
 }
 }
 
@@ -63,12 +67,16 @@ const [ecoSwampTents, setEcoSwampTents] = useState(0);
 function addEcoSwamp(){
   if(ecoSwampTents < 4){
   setEcoSwampTents(old => old+1);
+  setNumOfTents(old => old+1)
+
 }
 }
 
 function subtractEcoSwamp(){
   if(ecoSwampTents > 0){
   setEcoSwampTents(old => old-1);
+  setNumOfTents(old => old-1)
+
 }
 }
 
@@ -82,12 +90,16 @@ function subtractEcoSwamp(){
  function addSwampLuxForTwo(){
    if(swampLuxForTwo < 4){
    setSwampLuxForTwo(old => old+1);
+   setNumOfTents(old => old+1)
+
  }
  }
  
  function subtractSwampLuxForTwo(){
    if(swampLuxForTwo > 0){
    setSwampLuxForTwo(old => old-1);
+   setNumOfTents(old => old-1)
+
  }
  }
 
@@ -101,14 +113,21 @@ function subtractEcoSwamp(){
  function addSwampLuxForThree(){
    if(swampLuxForThree < 4){
    setSwampLuxForThree(old => old+1);
+   setNumOfTents(old => old+1)
  }
  }
  
  function subtractSwampLuxForThree(){
    if(swampLuxForThree > 0){
    setSwampLuxForThree(old => old-1);
+   setNumOfTents(old => old-1)
+
  }
  }
+
+ // store num of tents selected, despite which ones 
+
+ const [numOfTents, setNumOfTents] = useState(0);
 
 
 
@@ -284,9 +303,11 @@ return (
 
         <Link href="/buyingStage/ticketHolderInfo"
         >
-        <Button 
-        title="NEXT STEP"
-        />
+         <button className={styles.nextStepBtn}
+          disabled={numOfTents == 0 ? true : false}
+          >
+            NEXT STEP
+          </button>
         </Link>
       </div>
      
