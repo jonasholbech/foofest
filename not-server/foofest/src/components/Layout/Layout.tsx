5ece879d46d7f8eb6809f8b99e1cfbd15b33499c
Sabrina S <70270413+raptorp@users.noreply.github.com>
2023-05-27 15:34:19 +0200

    completed

--- not-server/foofest/src/components/Layout/Layout.tsx
+++ not-server/foofest/src/components/Layout/Layout.tsx
@@ -3,15 +3,12 @@ import TicketsContext from "@/context/ticketsContext";
 import Timer from "../Timer/Timer";
 
 import law from "./Layout.module.css";
-import Link from "next/link";
 
 import { useState } from "react";
 
-import { FaFacebookF, FaSpotify, FaTiktok } from "react-icons/fa";
-import { RiInstagramFill } from "react-icons/ri";
-import { DiMozilla } from "react-icons/di";
-
 import NavBurger from "../NavBurger/NavBurger";
+import BottomNav from "../BottomNav/BottomNav";
+
 const Layout = ({ children }) => {
   // burger menu control
   const [navOpen, setNavOpen] = useState(false);
@@ -28,55 +25,8 @@ const Layout = ({ children }) => {
         {globalValues.howManyTickets === 0 ? "" : <Timer />}
       </header>
       <main>{children}</main>
-      <footer className={law.footContainer}>
-        <div className={law.bottomNav}>
-          <div className={law.logoAndSoMe}>
-            <h1 className={law.logoTxt}>SwampFest</h1>
-          </div>
-
-          {/* bottom nav container */}
-          <ul className={law.bottomNavUl}>
-            <li>
-              <a href="">Contact</a>
-            </li>
-            <li>
-              <a href="">Press</a>
-            </li>
-            <li>
-              <a href="">Work with us</a>
-            </li>
-            <li>
-              <a href="">Data policy</a>
-            </li>
-          </ul>
-
-          <div className={law.SoMeBottomNav}>
-            {/* SoMe icon container */}
-            <a href="#_">
-              <FaFacebookF size={24} />
-            </a>
-
-            <a href="">
-              <RiInstagramFill size={26} />
-            </a>
-            <a href="#_">
-              <FaSpotify size={24} />
-            </a>
-            <a href="#_">
-              <FaTiktok size={24} />
-            </a>
-          </div>
-          <div className={law.sponsorsContainer}>
-            <p>SwampFest is sponsoret by</p>
-            <DiMozilla size={60} />
-            <DiMozilla size={60} />
-            <DiMozilla size={60} />
-          </div>
-        </div>
-
-        <p className={law.examProject}>
-          SwampFest is an exam project for 3rd semester on MMD KEA
-        </p>
+      <footer>
+        <BottomNav />
       </footer>
     </div>
   );
