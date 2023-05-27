9b96bb20b457a9bdbec827a80655b05d0c5ab0d8
Sabrina S <70270413+raptorp@users.noreply.github.com>
2023-05-27 19:08:08 +0200

    solved random path problem that affected all pages

--- not-server/foofest/src/pages/index.js
+++ not-server/foofest/src/pages/index.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import MainTitle from "@/components/MainTitle/MainTitle";
+import styles from "../styles/Home.module.css";
+import MainTitle from "../components/MainTitle/MainTitle";
 import stayOut from "../public/imgs/stayout.jpg";
-import SecondaryTitle from "@/components/SecondaryTitle/SecondaryTitle";
+import SecondaryTitle from "../components/SecondaryTitle/SecondaryTitle";
 
 const inter = Inter({ subsets: ["latin"] });
 
