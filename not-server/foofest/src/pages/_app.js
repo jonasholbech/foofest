c3e6a17a83908005a80e4a4c7d521496bf3bc385
Federico Barbieri <federicobarbieri54@gmail.com>
2023-05-28 13:53:23 -0300

    fulfill reservation + upload info to database ok

--- not-server/foofest/src/pages/_app.js
+++ not-server/foofest/src/pages/_app.js
@@ -27,7 +27,10 @@ export default function App({ Component, pageProps }) {
   const [globalVat, setGlobalVat] = useState(0);
 
   // global delivery object
-  const [deliveryObject, setDeliveryObject] = useState(null)
+  const [deliveryObject, setDeliveryObject] = useState()
+
+  // Save reservation ID globally
+  const [globalReservationId, setGlobalReservationId] = useState(null)
 
   return (
     <>
@@ -50,7 +53,9 @@ export default function App({ Component, pageProps }) {
           selectedCamp,
           setSelectedCamp,
           deliveryObject, 
-          setDeliveryObject
+          setDeliveryObject,
+          globalReservationId,
+          setGlobalReservationId
         }}
       >
         <Layout>
--- not-server/foofest/src/pages/buyingStage/checkout/index.js
+++ not-server/foofest/src/pages/buyingStage/checkout/index.js
@@ -39,7 +39,7 @@ function checkout() {
 
     globalMoneyContext.setDeliveryObject(user);
 
-  })
+  }, [user])
 
 
   
