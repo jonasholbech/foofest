7ed3c8f03c7994c58b1566e05878f52280e97d78
Sabrina S <70270413+raptorp@users.noreply.github.com>
2023-05-29 16:33:23 +0200

    Styling of band names + toggle expand

--- not-server/foofest/src/components/Artist/Artist.tsx
+++ not-server/foofest/src/components/Artist/Artist.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import artist from "./Artist.module.css";
 import { useState } from "react";
 
+import { BsChevronDown, BsChevronUp } from "react-icons/Bs";
+
 function Artist({ name, children }) {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
@@ -11,7 +13,14 @@ function Artist({ name, children }) {
         className={artist.collapseDivBtn}
         onClick={() => setIsCollapsed(!isCollapsed)}
       >
-        <p>{name}</p> <span>{isCollapsed ? "+" : "-"} </span>
+        <p>{name}</p>{" "}
+        <span>
+          {isCollapsed ? (
+            <BsChevronDown size={30} />
+          ) : (
+            <BsChevronUp size={30} />
+          )}
+        </span>
       </div>
 
       {!isCollapsed && children}
